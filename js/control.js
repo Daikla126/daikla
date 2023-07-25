5b0b9d6badf26c6153ee7467934f298584e45cde
Daikla126 <140496753+Daikla126@users.noreply.github.com>
2023-07-25 16:11:45 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.element', {
+    strings: ["Web Designer.", "Code Developer.","Apps Designer.","Film Maker"],
+ typeSpeed:10,
+ backSpeed:100,
+ loop:true,
+ });
+ $('.counter').countUp();
